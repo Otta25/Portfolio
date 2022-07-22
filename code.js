3bb74df631c38d4d3b4de876da84a5384f44e38f
Otta25 <juanmanuelottado@gmail.com>
2022-07-22 05:51:01 -0300

    Porfolio
    final portfolio 2022

---
+++ code.js
@@ -0,0 +1,28 @@
+
+let slider = document.querySelector("#slider");
+let contador = 0;
+window.sr = ScrollReveal()
+
+setInterval(cambiarImagen, 6000);
+
+function cambiarImagen() {
+ contador++
+ slider.style.backgroundImage=`url(assets/slider${contador}.jpg)`
+ if(contador===4){
+    contador=0
+ }
+};
+
+
+sr.reveal('header',{
+   duration:1000,
+   origin:'bottom',
+   distance:'-100px'
+})
+  
+sr.reveal('#info_contacto',{
+   duration:1000,
+   origin:'bottom',
+   distance:'-100px'
+})
+  
