355eeb561cf085c5cff809b5c82121d131794a92
Otta25 <juanmanuelottado@gmail.com>
2022-07-28 20:02:13 -0300

    header hide

--- code.js
+++ code.js
@@ -1,9 +1,26 @@
-
+let header = document.querySelector('header')
 let slider = document.querySelector("#slider");
 let contador = 0;
 window.sr = ScrollReveal()
 
 setInterval(cambiarImagen, 6000);
+
+window.addEventListener('scroll', hideHeader)
+
+
+
+function hideHeader(){
+   if(scrollY>300){
+      header.style.backgroundColor='transparent'
+      header.style.boxShadow='none'
+      console.log('HOLA')
+   }
+   else{
+      header.style.backgroundColor='rgb(2, 2, 51)'
+      header.style.boxShadow='0.1rem 0rem 1rem black'
+   }
+}
+
 
 function cambiarImagen() {
  contador++
